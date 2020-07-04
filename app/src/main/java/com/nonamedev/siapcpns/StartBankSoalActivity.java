6566531b74aab5e54a5636403530e98d01e55730
root <wicakhendrik@gmail.com>
2020-07-04 21:52:23 +0700

    penambahan simulasi

--- app/src/main/java/com/nonamedev/siapcpns/StartBankSoalActivity.java
+++ app/src/main/java/com/nonamedev/siapcpns/StartBankSoalActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.nonamedev.siapcpns.ui.banksoal.BankSoalFragment;
+import com.nonamedev.siapcpns.ui.simulasi.SimulasiFragment;
 
 public class StartBankSoalActivity extends AppCompatActivity {
     public static int id_tipe_soal;
@@ -20,36 +21,64 @@ public class StartBankSoalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start_bank_soal);
         textViewStart = findViewById(R.id.textViewStart);
         if ( BankSoalFragment.id == 1) {
+            id_tipe_soal = BankSoalFragment.id;
             setTitle("TIU Paket A");
             textViewStart.setText("TIU Paket A");
         }
         else if ( BankSoalFragment.id == 2) {
+            id_tipe_soal = BankSoalFragment.id;
             setTitle("TIU Paket B");
             textViewStart.setText("TIU Paket B");
         }
         else if ( BankSoalFragment.id == 3) {
+            id_tipe_soal = BankSoalFragment.id;
             setTitle("TIU Paket C");
             textViewStart.setText("TIU Paket C");
         }
         else if ( BankSoalFragment.id == 4) {
+            id_tipe_soal = BankSoalFragment.id;
             setTitle("TIU Paket D");
             textViewStart.setText("TIU Paket D");
         }
         else if ( BankSoalFragment.id == 5) {
+            id_tipe_soal = BankSoalFragment.id;
             setTitle("TWK Paket A");
             textViewStart.setText("TWK Paket A");
         }
         else if ( BankSoalFragment.id == 6) {
+            id_tipe_soal = BankSoalFragment.id;
             setTitle("TWK Paket B");
             textViewStart.setText("TWK Paket B");
         }
         else if ( BankSoalFragment.id == 7) {
+            id_tipe_soal = BankSoalFragment.id;
             setTitle("TWK Paket C");
             textViewStart.setText("TWK Paket C");
         }
         else if ( BankSoalFragment.id == 8) {
+            id_tipe_soal = BankSoalFragment.id;
             setTitle("TWK Paket D");
             textViewStart.setText("TWK Paket D");
+        }
+        else if (SimulasiFragment.id == 1) {
+            id_tipe_soal = SimulasiFragment.id;
+            setTitle("Simulasi Paket 1");
+            textViewStart.setText("Simulasi Paket 1");
+        }
+        else if (SimulasiFragment.id == 3) {
+            id_tipe_soal = SimulasiFragment.id;
+            setTitle("Simulasi Paket 2");
+            textViewStart.setText("Simulasi Paket 2");
+        }
+        else if (SimulasiFragment.id == 5) {
+            id_tipe_soal = SimulasiFragment.id;
+            setTitle("Simulasi Paket 3");
+            textViewStart.setText("Simulasi Paket 3");
+        }
+        else if (SimulasiFragment.id == 7) {
+            id_tipe_soal = SimulasiFragment.id;
+            setTitle("Simulasi Paket 4");
+            textViewStart.setText("Simulasi Paket 4");
         }
 
         Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
@@ -62,7 +91,6 @@ public class StartBankSoalActivity extends AppCompatActivity {
     }
 
     private void startQuiz() {
-        id_tipe_soal = BankSoalFragment.id;
         Intent intent = new Intent(StartBankSoalActivity.this, BankSoalActivity.class);
 
         startActivity(intent);
--- app/src/main/java/com/nonamedev/siapcpns/ui/simulasi/SimulasiFragment.java
+++ app/src/main/java/com/nonamedev/siapcpns/ui/simulasi/SimulasiFragment.java
@@ -2,6 +2,7 @@ package com.nonamedev.siapcpns.ui.simulasi;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.nonamedev.siapcpns.R;
+import com.nonamedev.siapcpns.StartBankSoalActivity;
 
 public class SimulasiFragment extends Fragment {
 
     private SimulasiViewModel mViewModel;
-
+    public static int id;
     public static SimulasiFragment newInstance() {
         return new SimulasiFragment();
     }
@@ -25,7 +28,44 @@ public class SimulasiFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_simulasi, container, false);
+        View root = inflater.inflate(R.layout.fragment_simulasi, container, false);
+        ImageButton btn1 = (ImageButton) root.findViewById(R.id.imageButton1Sim);
+        ImageButton btn2 = (ImageButton) root.findViewById(R.id.imageButton2Sim);
+        ImageButton btn3 = (ImageButton) root.findViewById(R.id.imageButton3Sim);
+        ImageButton btn4 = (ImageButton) root.findViewById(R.id.imageButton4Sim);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = 1;
+                Intent intent = new Intent(getActivity(), StartBankSoalActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = 3;
+                Intent intent = new Intent(getActivity(), StartBankSoalActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = 5;
+                Intent intent = new Intent(getActivity(), StartBankSoalActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = 7;
+                Intent intent = new Intent(getActivity(), StartBankSoalActivity.class);
+                startActivity(intent);
+            }
+        });
+        return root;
     }
 
     @Override
